6226cef848e3c6211fc4dccc60a26c24b9c7ebc6
SnehaPoonacha <91415595+SnehaPoonacha@users.noreply.github.com>
2022-04-21 22:20:31 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -9,3 +9,11 @@ trainer = ChatterBotCorpusTrainer(chatBot)
 
 
 trainer.train("chatterbot.corpus.english")
+
+print("Hi, I am ChatBot")
+
+
+while True:
+
+    query = input(">>>")
+        print(chatBot.get_response(Statement(text=query, search_text=query)))
